7bef6e8453d7d526a1e56ff5bc2ff494dac974fc
BrunoDiasLS <brunodlopes82@globo.com>
2022-09-15 18:17:21 -0300

    Index.mtml e index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * - Tipos primitivos
+ * - Variáveis no Javascript
+ * - Operadores matamáticos Javascript
+ */
+
+//string = textos
+//number = números
+//boolean = booleano true ou false
+console.log("Hello World") //serve pra escrever mensagem no console
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+//variáveis
+let mensagem = "Minha mensagem" // declaração de variável 
+
+console.log(mensagem) //Minha mensagem 
+
+mensagem = 'SoulCode BC25 JFS'
+
+console.log(mensagem) //SoulCode BC25 JF
